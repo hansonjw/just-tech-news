523b35f91465fd414156658bfdebee8d98c50b04
hansonjw <hansonjw@gmail.com>
2020-10-20 21:21:39 -0500

    create login page and logout features

--- server.js
+++ server.js
@@ -5,9 +5,23 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -23,4 +37,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
